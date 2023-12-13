55b54a177da406a1e6af5f2ae00a39f38c7dc969
Alex <Alexanderstubmichelsen@gmail.com>
2023-12-13 16:22:19 +0100

    isLoggedIn is now storedIsLoggedIn
    In local starage

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import facade from './util/apiFacade';
 import { Link } from 'react-router-dom';
 
-function App({setIsLoggedIn, isLoggedIn}) {
+const App = ({ setIsLoggedIn }) => {
   const init = { username: '', password: '' };
   const [loginCredentials, setLoginCredentials] = useState(init);
-  const [dataFromServer, setDataFromServer] = useState('Loading...');
-
-  useEffect(() => {
-    facade.fetchData('hotels', 'GET')
-    .then((data) => setDataFromServer(data));
-  }, [isLoggedIn]);
+  const isLoggedInStored = localStorage.getItem('isLoggedIn') === 'true';
 
   const performLogin = (evt) => {
     evt.preventDefault();
     facade.login(
       loginCredentials.username,
       loginCredentials.password,
-      setIsLoggedIn
+      () => {
+        setIsLoggedIn(true); // Set the application state to logged in
+        localStorage.setItem('isLoggedIn', 'true'); // Store in localStorage
+      }
     );
   };
+
   const onChange = (evt) => {
     setLoginCredentials({
       ...loginCredentials,
@@ -28,39 +27,43 @@ function App({setIsLoggedIn, isLoggedIn}) {
     });
   };
 
+  const handleLogout = () => {
+    setIsLoggedIn(false); // Set the application state to logged out
+    localStorage.setItem('isLoggedIn', 'false'); // Update localStorage
+  };
+
   return (
     <>
-      <div>
-        <h1>Login demo</h1>
-
-        <form onChange={onChange}>
-          <input placeholder="User Name" id="username" />
-          <input placeholder="Password" id="password" />
-          <button onClick={performLogin}>Login</button>
-        </form>
-
+      <body>
         <div>
-          {isLoggedIn ? (
-            <div>
-              <p>Du er logget ind, {facade.getUserName()}</p>
-              <button onClick={() => facade.logout(setIsLoggedIn)}>
-                Log out
-              </button>
-              {dataFromServer.map((hotel) => (
-                <p key={hotel.id}>{hotel.hotelName}</p>
-              ))}
+          <h1>Login</h1>
+
+          <div>
+            {isLoggedInStored ? (
               <div>
-                <Link to="/images" >Images <br/></Link>
-                <Link to="/savedImg" >Saved Images</Link>
+                <p>Du er logget ind, {facade.getUserName()}</p>
+                <button onClick={handleLogout}>
+                  Log out
+                </button>
+                <div>
+                  <Link to="/images" >Images <br/></Link>
+                  <Link to="/savedImg" >Saved Images</Link>
                 </div>
-            </div>
-          ) : (
-            <p>Log på for at være med.</p>
-          )}
+              </div>
+            ) : (
+
+<form className='loginform' onChange={onChange}>
+            <input placeholder="User Name" id="username" />
+            <input placeholder="Password" id="password" />
+            <button onClick={performLogin}>Login</button>
+          </form>
+
+            )}
+          </div>
         </div>
-      </div>
+      </body>
     </>
   );
-}
+};
 
 export default App;
