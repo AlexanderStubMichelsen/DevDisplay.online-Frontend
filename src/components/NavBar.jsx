69677d5b24b39bf964c1c14fbd367153c9945309
Alex <Alexanderstubmichelsen@gmail.com>
2024-07-12 18:31:53 +0200

    AdminSavedImages are removed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -58,11 +58,6 @@ function NavBar() {
               </LinkContainer>
             )}
             {userRoles.includes('admin') && (
-              <LinkContainer to="/admin/pictures" onClick={() => setExpanded(false)}>
-                <Nav.Link>Admin Pictures</Nav.Link>
-              </LinkContainer>
-            )}
-            {userRoles.includes('admin') && (
               <LinkContainer to="/admin/users" onClick={() => setExpanded(false)}>
                 <Nav.Link>Admin Users</Nav.Link>
               </LinkContainer>
