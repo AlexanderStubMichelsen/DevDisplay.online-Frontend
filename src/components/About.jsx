dfffaeeeb74da8e1546e9a7a82d2fb34f63bb2ff
Alex <Alexanderstubmichelsen@gmail.com>
2025-04-29 15:20:50 +0200

    They Took Our Job

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,26 +4,24 @@ import NavBar from "./NavBar"; // Import NavBar component
 import animationVideo from "../assets/animation.mp4"; // Import the video file
 
 const About = () => {
-
   return (
     <>
       <NavBar />
       <div className="about">
-        <video
-          controls
-          className="background-video"
-        >
+        <video controls className="background-video">
           <source src={animationVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
         {/* Add a button for playing an MP3 file */}
         <div className="audio-container">
-          <iframe width="110"
-          height="200"
-          src="https://www.myinstants.com/instant/south-park-they-took-our-job/embed/"
-          title="South Park 'They Took Our Job' Sound Button">
-          </iframe>
+          <iframe
+            width="110"
+            height="186"
+            src="https://www.myinstants.com/instant/south-park-they-took-our-job/embed/"
+            title="South Park They Took Our Job Sound"
+            style={{ border: "none", overflow: "hidden" }}
+          />
         </div>
       </div>
     </>
