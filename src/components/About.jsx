d890c3cdd266302aaf5281fcdbccceade9c9ed41
Alex <Alexanderstubmichelsen@gmail.com>
2025-04-29 13:51:47 +0200

    Lint

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import "../css/About.css"; // Optional: Add a CSS file for styling
 import NavBar from "./NavBar"; // Import NavBar component
 import animationVideo from "../assets/animation.mp4"; // Import the video file
