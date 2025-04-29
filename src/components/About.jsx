3e143d80ae66241b9b60def2af5098e13680ef0c
Alex <Alexanderstubmichelsen@gmail.com>
2025-04-29 14:21:55 +0200

    Opacity 0.8

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,20 +12,8 @@ const About = () => {
         <video
           controls
           className="background-video"
-          style={{
-            width: "640px", // Medium width
-            height: "360px", // Medium height
-            borderRadius: "10px", // Optional: Rounded corners
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Optional: Add a shadow
-          }}
         >
           <source src={animationVideo} type="video/mp4" />
-          <track
-            src="../assets/captions.vtt"
-            kind="subtitles"
-            srcLang="en"
-            label="English"
-          />
           Your browser does not support the video tag.
         </video>
 
@@ -34,8 +22,6 @@ const About = () => {
           <iframe width="110"
           height="200"
           src="https://www.myinstants.com/instant/south-park-they-took-our-job/embed/"
-          frameBorder="0"
-          scrolling="no"
           title="South Park 'They Took Our Job' Sound Button">
           </iframe>
         </div>
