15d6df3ea3f071bf17addaa07224cf5d02064b8c
Alex <Alexanderstubmichelsen@gmail.com>
2025-05-19 11:55:56 +0200

    Lint/help.jsx/css

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,29 +1,27 @@
 import React from "react";
-import { useState } from "react";
 import "../css/Help.css";
 import NavBar from "./NavBar";
 
 function Help() {
-
   return (
     <>
       <NavBar />
       <div className="help">
-        <div className="help-cantainer">
-        <h1 className="help-title">Help</h1>
-        <p className="help-text">
-          This is a simple ideo and image search application that <br />
-          allows you to search for images using the Unsplash API. <br />
-          You can also save your favorite images and <br />
-          view them later.
-        </p>
-        <h2 className="help-subtitle">How to use the app</h2>
-        <ol className="help-list">
-          <li>Search for images using the search bar.</li>
-          <li>Click on an image to view it in full size.</li>
-          <li>Click on the "Save" button to save an image.</li>
-          <li>View your saved images in the "Saved Images" section.</li>
-        </ol>
+        <div className="text-container">
+          <h1 className="help-title">Help</h1>
+          <p className="help-text">
+            This is a simple video and image search application that <br />
+            allows you to search for images using the Unsplash API. <br />
+            You can also save your favorite images and <br />
+            view them later.
+          </p>
+          <h2 className="help-subtitle">How to use the app</h2>
+          <ol className="help-list">
+            <li>Search for images using the search bar.</li>
+            <li>Click on an image to view it in full size.</li>
+            <li>Click on the &quot;Save&quot; button to save an image.</li>
+            <li>View your saved images in the &quot;Saved Images&quot; section.</li>
+          </ol>
         </div>
       </div>
     </>
