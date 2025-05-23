81d7006b0d16145e934d62ecc1dd0149b258cce7
Alex <Alexanderstubmichelsen@gmail.com>
2025-05-23 10:09:59 +0200

    Remove console in prod.

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import removeConsole from 'vite-plugin-remove-console';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    removeConsole()
+  ]
+});
