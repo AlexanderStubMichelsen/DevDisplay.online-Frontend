d7a728105ca062b4481b79ebacd9c87dd6f85f87
Alex <Alexanderstubmichelsen@gmail.com>
2025-07-07 14:40:37 +0200

    Lint

--- src/components/pages/NoMatch.jsx
+++ src/components/pages/NoMatch.jsx
@@ -22,8 +22,8 @@ const NoMatch = () => {
             <div className="error-number">404</div>
             <h1 className="error-title">Page Not Found</h1>
             <p className="error-description">
-              The page you're looking for doesn't exist or has been moved. Let's
-              get you back on track!
+              The page you&apos;re looking for doesn&apos;t exist or has been
+              moved. Let&apos;s get you back on track!
             </p>
 
             <div className="error-actions">
