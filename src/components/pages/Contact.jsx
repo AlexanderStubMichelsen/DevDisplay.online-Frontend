ba0e8f845645097b5c41e965fba4b6af391fc7e4
Alex <Alexanderstubmichelsen@gmail.com>
2025-08-22 09:23:38 +0200

    Lint empty pattern after removal of onsubmit pattern

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -9,7 +9,7 @@ const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
-const ContactForm = ({ }) => {
+const ContactForm = () => {
   const [form, setForm] = useState({
     name: "",
     email: "",
