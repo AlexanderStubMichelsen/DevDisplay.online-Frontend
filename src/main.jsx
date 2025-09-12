a3bb38c3f81dda3337d16e866a66827144a82840
Alex <Alexanderstubmichelsen@gmail.com>
2025-09-12 12:44:23 +0200

    Lint

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // filepath: c:\Projekter\Maskinen\src\main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet";
 import Root from "./Root.jsx"; // Import the Root component
 import "./index.css";
 
